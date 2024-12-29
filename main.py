4244a4883235d5328a9b0c54b41c13f6fbef0079
charliechen114514 <725610365@qq.com>
2024-12-29 17:47:35 +0800

    Setup projects

--- main.py
+++
@@ -1,9 +0,0 @@
-from AutoDatesetGeneratorWindow import AutoDatesetGeneratorWindow
-from PyQt6.QtWidgets import QApplication
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = AutoDatesetGeneratorWindow()
-    window.show()
-    sys.exit(app.exec())
